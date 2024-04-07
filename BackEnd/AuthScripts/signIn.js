0c62e9093fd2a9d5456b812ca6aacce094838b9f
fsiddiqi03 <farissiddiqi@gmail.com>
2024-04-07 01:28:22 -0500

    created sign in js file

---
+++ BackEnd/AuthScripts/signIn.js
@@ -0,0 +1,47 @@
+const AmazonCognitoIdentity = require('amazon-cognito-identity-js');
+
+// Cognito User Pool Id and App Client ID
+const poolData = {
+    UserPoolId: 'us-east-2_PbmKiUp9w', // Your Cognito User Pool ID
+    ClientId: '5mqnor5ufuojmi9c8q7dq9tfm0', // Your Cognito App Client ID
+};
+
+const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+
+document.getElementById('signInForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    
+    const authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails({
+        Username: username,
+        Password: password,
+    });
+
+    const userData = {
+        Username: username,
+        Pool: userPool,
+    };
+    const cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
+    
+    cognitoUser.authenticateUser(authenticationDetails, {
+        onSuccess: function(result) {
+
+            // Storing tokens in sessionStorage
+            sessionStorage.setItem('accessToken', result.getAccessToken().getJwtToken());
+            sessionStorage.setItem('idToken', result.getIdToken().getJwtToken()); 
+
+
+            const sub = result.idToken.payload.sub;
+            sessionStorage.setItem('userSub', sub); // used for get/put requests 
+
+            console.log('Sign-in successful!');
+            window.location.href = "calendar.html";
+
+        },
+        onFailure: function(err) {
+            console.error('Sign-in failed:', err);
+        },
+    });
+});
+
