d245e377dd2283516d2ab7cd169e45cfc2a74104
fsiddiqi03 <farissiddiqi@gmail.com>
2024-04-07 01:25:52 -0500

    created cognito tf file

---
+++ BackEnd/cognito.tf
@@ -0,0 +1,39 @@
+resource "aws_cognito_user_pool" "user_pool" {
+  name = "user-pool"
+
+  username_attributes = ["email"]
+  auto_verified_attributes = []
+  password_policy {
+    minimum_length = 6
+  }
+
+  
+
+  schema {
+    attribute_data_type      = "String"
+    developer_only_attribute = false
+    mutable                  = true
+    name                     = "email"
+    required                 = true
+
+    string_attribute_constraints {
+      min_length = 1
+      max_length = 256
+    }
+  }
+}
+
+resource "aws_cognito_user_pool_client" "client" {
+  name = "cognito-client"
+
+  user_pool_id = aws_cognito_user_pool.user_pool.id
+  generate_secret = false
+  refresh_token_validity = 90
+  prevent_user_existence_errors = "ENABLED"
+  explicit_auth_flows = [
+    "ALLOW_REFRESH_TOKEN_AUTH",
+    "ALLOW_USER_PASSWORD_AUTH",
+    "ALLOW_ADMIN_USER_PASSWORD_AUTH"
+  ]
+  
+}
